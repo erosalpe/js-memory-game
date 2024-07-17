132f8e24a275a97eab7a7d3b418babb3da2e325e
erosalpe <eros.alpegiani@gmail.com>
2024-07-17 13:12:18 +0200

    feat(main.js) added grid generation logic)

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const grid = document.getElementById("container-cards");
 let gridValue = [];
 let randomNumber;
+let errors = 0;
 
 //creates an array with numbers 1 to 12
 for(let i = 0; i < 12; i++){
@@ -11,3 +12,26 @@ for(let i = 0; i < 12; i++){
 }
 
 console.log(gridValue);
+
+
+//cards generation
+gridValue.forEach(function(number) {
+
+    //create a new div it will contain 1 card img
+    let newCard = document.createElement('div');
+    newCard.classList.add('card');
+    newCard.id = `${number}`;
+
+    //create a new img tag it will contain the back card img
+    let newImg = document.createElement('img');
+    newImg.src = './assets/images/back.png';
+    newImg.alt = 'card';
+
+    //add img tag to newCard
+    newCard.appendChild(newImg);
+
+    //add newCard to the cards grid
+    grid.appendChild(newCard);
+
+    console.log(number);
+});
